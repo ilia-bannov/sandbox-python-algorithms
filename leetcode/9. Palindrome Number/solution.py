eb22a0f9b506d02d39ec03df136a8e0726422c1e
Ilia Bannov <bannov.ilya@gmail.com>
2025-02-13 22:33:08 +0300

    add leetcode problem solutions

---
+++ leetcode/9. Palindrome Number/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        y = x
+        z = 0
+        while y > 0:
+            z = z*10 + (y % 10)
+            y //= 10
+
+        return z == x
